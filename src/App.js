d064a81e183104797346c7b827e63b0aeeb4e864
Hashir Baig <hashirbaig456@gmail.com>
2023-01-17 18:28:30 +0500

    Added main and changed app name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { ChakraProvider, theme } from "@chakra-ui/react"
 import Header from "./components/layout/Header"
+import Main from "./components/layout/Main"
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Header />
+      <Main>
+        <p>Main</p>
+      </Main>
     </ChakraProvider>
   )
 }
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="h-[80px] shadow-md bg-gray-900">
+    <header className="h-[80px] bg-gray-900">
       <div className="h-full flex items-center justify-center bg-inherit">
         <span className="text-gray-300 text-2xl font-semibold">Ready Or Not</span>
       </div>
